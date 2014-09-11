06780e44679937948edebd113cf0464dc0878560
Sergiusz <sergiusz.bleja@timgroup.com>
2014-09-11 14:27:22 +0100

    Wrapping in R-markdown to create a html document

--- Sep-2014-Dojo/ebola/ebola.R
+++ Sep-2014-Dojo/ebola/ebola.R
@@ -1,57 +1,78 @@
 require('ggplot2')
 
-# set the full path here:
-ebola <-read.csv("{{set full path here}}/Ebola - Countries.csv")
+run <- function() {
 
-head(ebola)
+  # set the full path here:
+  ebola <-read.csv("{{set full path here}}/Ebola - Countries.csv")
 
+  head(ebola)
 
-ggplot(ebola,aes(x=Day,y=GuinSus))+geom_point() + ggtitle("Suspected cases in Guinea")
+  exploratoryPlots(ebola)
 
-ebola$death.per.day=c(0,diff(ebola$GuinSus))
-ggplot(ebola,aes(x=Day,y=death.per.day))+geom_point() + ggtitle("Deaths per day in Guinea")
-ggplot(ebola,aes(x=Day,y=death.per.day))+geom_point() + ggtitle("Deaths per day in Guinea") + stat_smooth()
+  mod <- linearModel(ebola)
 
-# Compute total suspected cases
-ebola$totsus = ebola$GuinSus + ebola$LibSus + ebola$NigSus + ebola$SLSus + ebola$SenSus
-ggplot(ebola,aes(x=Day,y=totsus))+geom_point()+stat_smooth() + ggtitle("Total suspected cases")
+  ebola <- addPredictedValues(ebola, mod)
 
-ggplot(ebola,aes(x=Day)) + 
+  # extrapolate
+  intercept=mod$coefficients[1]
+  slope=mod$coefficients[2]
+
+  prd(200)
+
+  # Discover how long we have left
+  days.until.end.of.humanity(6e9)
+
+  # THE END (all dead)
+}
+
+exploratoryPlots <- function(ebola) {
+  print(ggplot(ebola,aes(x=Day,y=GuinSus))+geom_point() + ggtitle("Suspected cases in Guinea"))
+
+  ebola$death.per.day=c(0,diff(ebola$GuinSus))
+  print(ggplot(ebola,aes(x=Day,y=death.per.day))+geom_point() + ggtitle("Deaths per day in Guinea"))
+  print(ggplot(ebola,aes(x=Day,y=death.per.day))+geom_point() + ggtitle("Deaths per day in Guinea") + stat_smooth())
+
+  print(ggplot(ebola,aes(x=Day,y=totsus))+geom_point()+stat_smooth() + ggtitle("Total suspected cases"))
+
+  print(ggplot(ebola,aes(x=Day)) + 
   geom_point(aes(y=totsus, colour = "Total")) +
   geom_point(aes(y=GuinSus, colour = "Guinea")) + 
   scale_y_log10() +
-  ggtitle("Suspected in Total and Guinea")
+  ggtitle("Suspected in Total and Guinea"))
+}
 
-# create a simple linear model (~ is the 'equals' sign in an equation)
-mod = lm( log(ebola$totsus) ~ ebola$Day )
-# add a new column to the table containing the predicted total cases for that day using our model
-ebola$predicted = exp(c(0,predict(mod)))
 
-ggplot(ebola,aes(x=Day)) +
+computeTotalCases <- function(ebola) {
+  # Compute total suspected cases
+  ebola$totsus = ebola$GuinSus + ebola$LibSus + ebola$NigSus + ebola$SLSus + ebola$SenSus
+  ebola
+}
+linearModel <- function(ebola) {
+  # create a simple linear model (~ is the 'equals' sign in an equation)
+  mod = lm( log(ebola$totsus) ~ ebola$Day )
+}
+
+addPredictedValues <- function(ebola, mod) {
+  # add a new column to the table containing the predicted total cases for that day using our model
+  ebola$predicted = exp(c(0,predict(mod)))
+  ebola
+}
+
+plotPredictedVsActual <- function(ebola) {
+  ggplot(ebola,aes(x=Day)) +
   geom_point(aes(y=totsus, colour = "Total")) +
   geom_point(aes(y=predicted, colour = "Predicted")) +
   scale_y_log10() +
   ggtitle("Total and Predicted")
+}
 
-
-# extrapolate
-intercept=mod$coefficients[1]
-slope=mod$coefficients[2]
-
-# expess our model as a function that returns the predicted cases up to any given day
-prd=function(d) {
+# # expess our model as a function that returns the predicted cases up to any given day
+prd=function(d, intercept, slope) {
   exp(intercept + d*slope)
 }
 
-# on day 200, how many?
-prd(200)
-
 # Create a doomsday function
-days.until.end.of.humanity = function(totd) {
+days.until.end.of.humanity = function(totd, intercept, slope) {
   (log(totd)-intercept) / slope
 }
 
-# Discover how long we have left
-days.until.end.of.humanity(6e9)
-
-# THE END (all dead)
