27dc895ddf59b94187f34595823afafcaa62a81d
Sergiusz <sergiusz.bleja@timgroup.com>
2014-11-12 22:42:25 +0000

    Changes to Bayesian Poker

--- Oct-2014-Dojo/BayesianPoker.R
+++ Oct-2014-Dojo/BayesianPoker.R
@@ -19,10 +19,11 @@ y[J] ~ normal(mu, sigma);
 n= 16900
 poker_dat <- list(J = n, 
                   y = c(rnorm(n, 1.15, 0.05)),
+                  #                   sigma=c(rnorm(n, 1.61, 0.05)))
                   sigma=c(rnorm(n, 1.61, 0.05)))
 
 fit <- stan(model_code = poker.skill, data = poker_dat, 
             iter = 1000, chains = 4)
 
 plot(fit)
-3 - pnorm(0, mean = 0.39, sd = 2.76)
+1 - pnorm(0, mean = 0.39, sd = 2.76)
